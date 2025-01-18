d1371ddf4a637f40a456901821c046318c57468c
Himanshu <himanshubindal90340@gmail.com>
2025-01-18 14:13:52 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,40 +5,38 @@ import cors from "cors";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import { connectDB } from "./lib/db.js";
-import { app,server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Increase body size limit for both JSON and URL-encoded payloads
-app.use(express.json({ limit: '50mb' }));  // Increase limit for JSON payloads
-app.use(express.urlencoded({ limit: '50mb', extended: true }));  // Increase limit for URL-encoded data
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: "http://localhost:5173",  // Adjust this for your frontend
-  credentials: true
-}));
-
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// Start the server
 server.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
